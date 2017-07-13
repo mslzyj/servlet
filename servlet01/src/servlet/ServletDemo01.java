ccb348e7dd15238ef3fda0fc85f82ca62650a833
mslzyj <m15829082449@163.com>
2017-07-13 09:55:09 +0800

    first servlet01

---
+++ servlet01/src/servlet/ServletDemo01.java
@@ -0,0 +1,20 @@
+package servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ServletDemo01 extends HttpServlet {
+
+	
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		System.out.println("doGet方法被调用了");
+	}
+
+}
